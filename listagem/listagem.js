e05249757f8a3c5105a0135d311dbbe2cb1c72e9
MucaBordini <mucabordini@gmail.com>
2019-11-21 16:41:33 -0300

    Lista

--- listagem/listagem.js
+++ listagem/listagem.js
@@ -1,13 +1,21 @@
 var corpo_tabela = document.querySelector("tbody");
 
 //Função para listar os Posts cadastrados
-function listar() {
+function listar(pagina) {
     //Fazendo o request AJAX para o json-server
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var json = JSON.parse(this.response); //Salva um registro na variavel json
         var i;
+        var _pag;
+        if (json.length % 10 == 0){
+          _pag = Math.trunc(json.length/10);
+        } else {
+          _pag = Math.trunc(json.length/10) + 1;
+        }
+        
+        console.log(_pag);
         for(i = 0; i < json.length; i++){//For para preencher a tabela 
             var linha = document.createElement("tr");
             var id = document.createElement("th");
@@ -30,6 +38,6 @@ function listar() {
         
       }
     };
-    xhttp.open("GET", "http://localhost:3000/posts", true);
+    xhttp.open("GET", "http://localhost:3000/posts?_page="+ pagina, true);
     xhttp.send();
   }
